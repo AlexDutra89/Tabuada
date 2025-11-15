118071ca855a3aa428c304e35e6e51adcbf7c08c
LeDutra89 <ledutra3@gmail.com>
2025-11-14 22:47:44 -0300

    branch01

--- ProjetoTabuadaGit/src/Projetos/Tabuada.java
+++ ProjetoTabuadaGit/src/Projetos/Tabuada.java
@@ -1,10 +1,27 @@
 package Projetos;
 
+import java.util.Scanner;
+
 public class Tabuada {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Olá Mundo!!");
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Informe um número inteiro: ");
+		int numero1 = entrada.nextInt();
+		
+		System.out.println("Informe outro número inteiro: ");
+		int numero2 = entrada.nextInt();
+		
+		int resultado = numero1 * numero2;
+		
+		System.out.println(resultado);
+		
+		entrada.close();
+		
+		
+		
 	}
 
 }
